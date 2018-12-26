4c0aca4f616556ca4fd0ce7462f763d5809bc0ba
MrKaban <egor.iva.ser@gmail.com>
2018-12-26 15:49:49 +0300

    Labs is done

---
+++ Labs_1.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double a, z1, z2;
+	cout << "Ente a " << endl;
+	cin >> a;
+	z1 = cos(a) + sin(a) + cos(3 * a) + sin(3 * a);
+	z2 = 2 * sqrt(2)*cos(a)*sin((3.14 / 4) *(2 * a));
+	if (z1 == z2)
+		cout << z1 << " = " << z2;
+	else {
+		if (z1 < z2) {
+			cout << z1 << " < " << z2;
+		}
+		else {
+			cout << z1 << " > " << z2;
+		}
+	}
+	cin.get();
+	cin.get();
+	return 0;
+}
---
+++ Labs_2.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include <iostream> 
+#include<cmath> 
+
+using namespace std;
+
+double fact(int n) {
+	double res = 1;
+	for (int i = 1; i <= n; i++) {
+		res = res * i;
+	}
+	return res;
+}
+int main() {
+	double eps, f1 = 10000, f = 0, A, B;
+	float dx;
+	int i = 0, j = 0;
+	cout << "Enter A and B ";
+	cin >> A >> B;
+
+	cout << "Enter dx" << endl;
+	cin >> dx;
+	cout << "Enter eps ";
+	cin >> eps;
+	cout << "+--------+---------------+-+" << endl;
+	cout << "|" << "X" << "\t |" << "f(x)" << "\t " << "\t |N" << "|" << endl;
+	cout << "+--------+---------------+-+" << endl;
+	while (A <= B + dx) {
+		while (f1 >= eps) {
+			f1 = pow(A, i) / fact(i);
+			i += 2;
+			f = (f1*pow(-1, j)) + f;
+			j++;
+		}
+		cout << "|" << A << "\t |" << f << "\t |" << j << "|" << endl;
+		f1 = 1000;
+		f = 0; i = 0; j = 0;
+		A += dx;
+		cout << "+--------+---------------+-+" << endl;
+	}
+	cin.get();
+	cin.get();
+	return 0;
+}
